bd6d0b656b5a5be27a31e573aa1fa8f20268ffa9
Gustavo Alves Pacheco <gap1512@gmail.com>
2022-08-14 17:57:51 -0300

    feat: call api to create post

--- hooks/use-posts.js
+++ hooks/use-posts.js
@@ -3,12 +3,14 @@ import { useState } from "react";
 export function usePosts() {
   const [posts, setPosts] = useState([]);
   const [isLoadingPosts, setLoadingPosts] = useState(false);
+  const [isLoadingCreate, setLoadingCreate] = useState(false);
+
+  const baseApiUrl =
+    "https://itfpz6mc7f.execute-api.us-east-1.amazonaws.com/default";
 
   function getPosts() {
     setLoadingPosts(true);
-    fetch(
-      "https://itfpz6mc7f.execute-api.us-east-1.amazonaws.com/default/getPost/"
-    )
+    fetch(`${baseApiUrl}/getPost/`)
       .then((res) => res.json())
       .then((posts) => {
         setPosts(posts);
@@ -16,5 +18,26 @@ export function usePosts() {
       });
   }
 
-  return { isLoading: isLoadingPosts, posts, getPosts };
+  function createPost(data) {
+    setLoadingCreate(true);
+    fetch(`${baseApiUrl}/postPost`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        body: JSON.stringify(data),
+      },
+    })
+      .then((res) => res.json())
+      .then((post) => {
+        console.log(post);
+        setLoadingCreate(false);
+      });
+  }
+
+  return {
+    isLoading: isLoadingPosts || isLoadingCreate,
+    posts,
+    getPosts,
+    createPost,
+  };
 }
